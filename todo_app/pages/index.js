62a36960cf91610d6f840e7e0fba8df73602d30e
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-15 21:33:23 +0530

    create list

--- todo_app/pages/index.js
+++ todo_app/pages/index.js
@@ -1,8 +1,33 @@
-
+import { useState } from "react";
 
 const Home = () => {
+ const [items, setItems] = useState([
+   "Assignment 01",
+   "Assignment 02",
+   "Assignment 03",
+   "Quiz 01",
+   "Quiz 02",
+ ]);
   return (
-    <div>Hello World</div>
+    <div>
+      <h1>Todo App</h1>
+      
+      <div>
+        <input type="text" />
+        <button type="button">Add</button>
+      </div>
+
+      <ul>
+        {
+          items.map((item) => (
+            <li key={item}>{item}</li>
+          ))
+        }
+       
+      </ul>
+
+    </div>
+   
   )
 };
 export default Home;
