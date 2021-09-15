411e8b2ed04c7d6f5b8adf82eaf5e1d78fa10c0a
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-09-16 00:16:30 +0530

    apply toggle Function

--- todo_app/pages/index.js
+++ todo_app/pages/index.js
@@ -1,3 +1,4 @@
+import cx from "classnames";
 import { useState } from "react";
 import {v4 as uuidv4 } from "uuid"
 
@@ -60,8 +61,8 @@ const Home = () => {
 
       <ul>
         {
-          items.map(({id, message}) => (
-            <li key={id} onClick={() => handdleToggle(id)}>{message}</li>
+          items.map(({id, message , done}) => (
+            <li key={id} onClick={() => handdleToggle(id)} className= {cx("item", { done})}>{message}</li>
           ))
         }
        
